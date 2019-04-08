32d4d5bef1a3ecddd94f92d2a92b1ac537636d1c
csw <work@qq.com>
2019-04-08 17:20:47 +0800

    update linux common demo

--- linux/linux-common/time.cpp
+++ linux/linux-common/time.cpp
@@ -12,4 +12,15 @@ static void getTimeBuf(char * buf,size_t len){
 	return ;
 }
 
+int  getTimeBuf(char* szTime){
 
+	time_t now;
+	time(&now);
+		
+	struct tm* timenow;
+	timenow = localtime(&now);
+	int nLen = sprintf(szTime, "[%04d-%02d-%02d %02d:%02d:%02d] ",
+				   timenow->tm_year + 1900, timenow->tm_mon + 1, timenow->tm_mday,
+				   timenow->tm_hour, timenow->tm_min, timenow->tm_sec);
+	return (int)nLen;
+}
