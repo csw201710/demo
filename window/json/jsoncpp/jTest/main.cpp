4958eb42b724ccfded1cd1515b9e0f0b2669d74d
csw <work@qq.com>
2019-09-16 10:56:27 +0800

    add jsoncpp demo

---
+++ window/json/jsoncpp/jTest/main.cpp
@@ -0,0 +1,53 @@
+#include "json/json.h"
+#include <iostream>
+
+void readJson() {
+	using namespace std;
+	std::string strValue = "{\"name\":\"json\",\"array\":[{\"cpp\":\"jsoncpp\"},{\"java\":\"jsoninjava\"},{\"php\":\"support\"}]}";
+
+	Json::Reader reader;
+	Json::Value value;
+
+	if (reader.parse(strValue, value))
+	{
+		std::string out = value["name"].asString();
+		std::cout << out << std::endl;
+		const Json::Value arrayObj = value["array"];
+		for (unsigned int i = 0; i < arrayObj.size(); i++)
+		{
+			if (!arrayObj[i].isMember("cpp"))
+				continue;
+			out = arrayObj[i]["cpp"].asString();
+			std::cout << out;
+			if (i != (arrayObj.size() - 1))
+				std::cout << std::endl;
+		}
+	}
+}
+
+void writeJson() {
+	using namespace std;
+
+	Json::Value root;
+	Json::Value arrayObj;
+	Json::Value item;
+
+	item["cpp"] = "jsoncpp";
+	item["java"] = "jsoninjava";
+	item["php"] = "support";
+	arrayObj.append(item);
+
+	root["name"] = "json";
+	root["array"] = arrayObj;
+
+	root.toStyledString();
+	std::string out = root.toStyledString();
+	std::cout << out << std::endl;
+}
+
+int main(){
+	
+	readJson();
+	writeJson();
+	return 0;
+}
