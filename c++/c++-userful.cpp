0982a45717cdc2bbd4bb8e342450430890a4e306
csw <js@qq.com>
2020-06-22 20:56:28 +0800

    update c++ demo

---
+++ c++/c++-userful.cpp
@@ -0,0 +1,32 @@
+static std::string fmt(const char *pszFmt, ...)
+{
+#ifdef _WIN32
+	std::string str;
+	va_list args;
+	va_start(args, pszFmt);
+	{
+		int nLength = _vscprintf(pszFmt, args);
+		nLength += 1; //add again
+		std::vector<char> vectorChars(nLength);
+		_vsnprintf(vectorChars.data(), nLength, pszFmt, args);
+		str.assign(vectorChars.data());
+	}
+	va_end(args);
+	return str;
+#else
+  va_list ap;
+  va_start(ap, pszFmt);
+  char buf[2048];
+  int rv;
+  rv = vsnprintf(buf, sizeof(buf), pszFmt, ap);
+
+  if (rv < 0) {
+    buf[0] = '\0';
+  }
+  
+  va_end(ap);
+  return buf;
+#endif
+}
+
+
