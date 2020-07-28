a525a6f69f2d83883c1386d66f8e50b4af582a10
csw <js@qq.com>
2020-07-28 15:48:31 +0800

    update linux signal demo

--- linux/linux-signal/signal-alarm.cpp
+++ linux/linux-signal/signal-alarm.cpp
@@ -22,3 +22,39 @@ return 0;
 }
 
 
+/*
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+ 
+static int alarm_fired = 0;
+ 
+void ouch(int sig)
+{
+    alarm_fired = 1;
+}
+ 
+int main()
+{
+    // 关联信号处理函数
+    signal(SIGALRM, ouch);
+     
+    // 调用alarm函数，5秒后发送信号SIGALRM
+    alarm(5);
+     
+    // 挂起进程
+    pause();
+     
+    // 接收到信号后，恢复正常执行
+    if(alarm_fired == 1)
+    {
+        printf("Receive a signal %d\n", SIGALRM);
+    }
+ 
+    exit(0);
+}
+*/
+
+
