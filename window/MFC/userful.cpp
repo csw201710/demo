3279dbacaf7f78dd9b6a39f8c95f59aa1e241603
csw <js@qq.com>
2020-10-26 14:53:29 +0800

    update mfc demo

--- window/MFC/userful.cpp
+++ window/MFC/userful.cpp
@@ -79,3 +79,90 @@ WriteStatusLog (connection_t *c, const WCHAR *prefix, const WCHAR *line, BOOL fi
 }
 
 //=========================================================================================//
+
+
+
+
+
+
+
+
+
+//===========================根据进程id获取窗口句柄 start======================================//
+#include <windows.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <Psapi.h>
+#include <string.h>
+#pragma comment ( lib, "Psapi.lib" )
+
+
+///< 枚举窗口参数
+typedef struct
+{
+	HWND hwndWindow; // 窗口句柄
+	DWORD dwProcessID; // 进程ID
+}EnumWindowsArg;
+///< 枚举窗口回调函数
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam)
+{
+	EnumWindowsArg *pArg = (EnumWindowsArg *)lParam;
+	DWORD dwProcessID = 0;
+	// 通过窗口句柄取得进程ID
+	::GetWindowThreadProcessId(hwnd, &dwProcessID);
+	if (dwProcessID == pArg->dwProcessID)
+	{
+		pArg->hwndWindow = hwnd;
+		// 找到了返回FALSE
+		return FALSE;
+	}
+	// 没找到，继续找，返回TRUE
+	return TRUE;
+}
+// 通过进程ID获取窗口句柄
+HWND GetWindowHwndByPID(DWORD dwProcessID)
+{
+	HWND hwndRet = NULL;
+	EnumWindowsArg ewa;
+	ewa.dwProcessID = dwProcessID;
+	ewa.hwndWindow = NULL;
+	EnumWindows(EnumWindowsProc, (LPARAM)&ewa);
+	if (ewa.hwndWindow)
+	{
+		hwndRet = ewa.hwndWindow;
+	}
+	return hwndRet;
+}
+
+
+
+int main() {
+	HWND hwnd = 0;
+	HWND htmp = 0;
+	hwnd = GetWindowHwndByPID(19192);
+	printf("hwnd: %x", hwnd);
+        //maybe 忽略
+	while (htmp = GetParent(hwnd))
+	{
+		hwnd = htmp;
+	}
+	printf("hwnd: %x", hwnd);
+	SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+	system("pause");
+	return 9;
+}
+
+//===========================根据进程id获取窗口句柄 end======================================//
+
+
+
+
+
+
+
+
+
+
+
+
+
