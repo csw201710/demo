c306ec9a88e27b4608b244e9eede79338f00df0d
csw <js@qq.com>
2020-12-30 15:55:01 +0800

    update asn1 demo

--- gmssl/ASN1/asn1-func.cpp
+++ gmssl/ASN1/asn1-func.cpp
@@ -48,3 +48,42 @@ ASN1_OBJECT *ASN1_OBJECT_create(int nid, unsigned char *data, int len,
     return (OBJ_dup(&o));
 }
 
+
+
+
+
+
+/*
+asn1 第一个tag字节
+	BER_TYPE_BOOLEAN	0x01
+	BER_TYPE_INTEGER	0x02
+	BER_TYPE_BIT_STRING	0x03
+	BER_TYPE_OCTET_STRING	0x04
+	BER_TYPE_NULL	0x05
+	BER_TYPE_OID	0x06
+	BER_TYPE_SEQUENCE	0x30
+	BER_TYPE_SNMP_SET	0xA3
+*/
+// 根据asn1编码，解析处实际证书长度
+int calc_cert_length(uint8 cert_header[4])
+{
+	uint16 cert_len = -1;
+	//cert_header[0] == 0x30
+	if ((cert_header[1] & 0x80) != 0) //case more than 127 bytes
+	{
+		if ((cert_header[1] & 0x7f) != 0x02)//check the length word whether correct
+		{
+			return -1;
+		}
+		//calculate the actual length of certificate
+		cert_len = ((int)cert_header[2] << 8) + cert_header[3] + 4;
+	}
+	else    //case <=127 bytes,in fact,this will never act
+	{
+		cert_len = (cert_header[1] & 0x7f) + 2;
+	}
+
+	return cert_len;
+}
+
+
